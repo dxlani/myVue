f9338d59c4d6cebb20b888c8caa6599f26d705b2
dxlani <870188670@qq.com>
2018-03-14 15:48:15 +0800

    分割.vue

--- src/components/login/login.ts
+++ src/components/login/login.ts
@@ -1,14 +1,16 @@
 import  Component  from 'vue-class-component';
 import Vue from 'vue'
-// import "./login.css";
+import template from './login.vue'
 declare var bootbox:any;
 declare var $:any;
 
 @Component({
-  template: '<button class="btn btn-sm btn-danger" @click="onClick">Click!</button>'
+  // template: '<button class="btn btn-sm btn-danger" @click="onClick">Click!</button>'
+  name:'template',
+  mixins: [template]
 })
 export default class login extends Vue {
- 
+  message:string="hello vue";
   msg: string = 'Hello! dingxiaolin'
 
   onClick (): void {
---
+++ src/components/login/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div  id="login" >
+    <h1>{{ message }}</h1>
+    <button class="btn btn-xs btn-danger" @click="onClick">Click!</button>
+  </div>
+</template> 
+ <style src="./login.css" lang="css" scoped></style>
+
+
+
