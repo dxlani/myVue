0a9aa8932d90c626cc8b7db18795e91d83b050bf
dxlani <870188670@qq.com>
2018-05-11 16:48:55 +0800

    api接口优化

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import { Post, Get, Patch, Put, Delete,ax} from '../http/http'
 
-ax.defaults.baseURL='http://192.168.1.107:777/csp/';
-
+// ax.defaults.baseURL='http://192.168.1.107:777/csp/';
+ax.defaults.baseURL = "http://192.168.1.205:5000/csp/"
 export default {
     getPackage(data) {
         return Get('posts',data)
