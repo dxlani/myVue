01a6860d5e08442437c36daceca5dcdc84f023a2
Angelaj <960923175@qq.com>
2018-05-14 10:53:53 +0800

    change

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import router from './router'
 import Vuex from 'vuex'
 import store from './vuex/store'
 import { Component, Emit, Inject, Model, Prop, Provide, Vue, Watch } from 'vue-property-decorator'
-import * as echarts from "echarts"
 
 import navbar from './components/nav'
 import header from './components/header'
