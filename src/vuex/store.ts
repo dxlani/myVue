e8816d8be69c8298fbb6e742c055101c4d58d97e
丁晓林 <870188670@qq.com>
2018-05-14 16:33:17 +0800

    Update store.ts

--- src/vuex/store.ts
+++ src/vuex/store.ts
@@ -7,7 +7,7 @@ const store = new Vuex.Store({
   // 定义状态
   state: {
     author: 'Dingxiaolin',
-    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzaW5vIiwianRpIjoiN2YwNjIyNjMtYzBlMC00N2M4LTk4ODYtNjdlY2E3N2FhZjNlLDE0MDZkNTY5LTIzZjItNGJkMC1hODgzLTE4NTM4NjdkZTFjNSIsInNpZCI6IjIwMCIsImlhdCI6MTUyNjM1MDc1MCwibmJmIjoxNTI2MjY0MzUwLCJleHAiOjE1MjYzNTA3NTAsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6MTc3NDEiLCJhdWQiOiJjc3AifQ.RN7OcUfG9tTzg6XFcZqAd7SDjecTvzqMvZPjHBB_GTc',
+    token:'',
     isLogin:''
   },
   mutations: {
